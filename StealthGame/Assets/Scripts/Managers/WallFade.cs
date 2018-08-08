7785d3316fe743a777997170a21315a5780c39ad
Trent <contact@trentwilliamswanson.com>
2018-08-08 12:30:09 +1000

    Added wall fade

---
+++ StealthGame/Assets/Scripts/Managers/WallFade.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallFade : MonoBehaviour {
+
+	private Material standardMat;
+	
+	public Material fadeMat;
+	public Material[] wallList;
+
+	void Start () {
+		wallList = new Material[transform.childCount];
+
+		standardMat = wallList[0];
+
+		for (int i = 0; i < transform.childCount; i++) {
+			wallList[i] = transform.GetChild(i).GetComponent<Material>();
+		}
+	}
+
+	public void FadeWall() {
+		for (int i = 0; i < wallList.Length; i++) {
+			wallList[i] = fadeMat;
+		}
+	}
+
+	public void UnFadeWall() {
+		for (int i = 0; i < wallList.Length; i++) {
+			wallList[i] = standardMat;
+		}
+	}
+}
