78ed9457c0dfeaaa0fbd12fb21588f37eb63f64d
Kyle Taylor <kyle.jesse.taylor@gmail.com>
2018-08-27 14:17:36 +1000

    Added Wall Colliders, UI characters from 3 > 2, Kyle's Scene Restart

---
+++ StealthGame/Assets/Scripts/KyleCollision.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KyleCollision : MonoBehaviour {
+
+    public bool completed = false;
+    public List<GameObject> inside = new List<GameObject>();
+
+    public bool safe = false;
+    public GameObject safeOBJ;
+
+    void Start()
+    {
+        //Checks if this is the Safe
+        if (name == "Safe")
+        {
+            safe = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //If player
+        if (other.tag == "Player")
+        {
+            //Adds player to this object's list
+            inside.Add(other.gameObject);
+
+            //Safe
+            if (safe)
+            {
+                InteractSafe(other);
+                //Enter Safe
+                Debug.Log("Entered Safe");
+            }
+            //Extrance & Exit
+            else
+            {
+                InteractDoor();
+                //Enter Door
+                Debug.Log("Entered Door");
+            }
+
+        }
+    }
+
+    private void InteractSafe(Collider other)
+    {
+        //Turns off particle
+        other.transform.GetChild(1).gameObject.SetActive(false);
+        //Change mesh to open door
+        safeOBJ.GetComponent<MeshRenderer>().enabled = false;
+        //^^^REPLACE^^^
+
+        //Completed that collider
+        completed = true;
+        transform.parent.GetComponent<KylesEndScene>().safeLooted = true;
+    }
+
+    private void InteractDoor()
+    {
+        //Completed that collider
+        completed = true;
+        transform.parent.GetComponent<KylesEndScene>().triggerEntered = true;
+    }
+
+}
---
+++ StealthGame/Assets/Scripts/KylesEndScene.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KylesEndScene : MonoBehaviour {
+
+    [Header("Triggers")]
+    public GameObject entrance;
+    public GameObject exit;
+    public GameObject safe;
+    [HideInInspector]
+    public bool safeLooted = false;
+
+    public bool triggerEntered = false;
+
+    private List<GameObject> players = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+        foreach(GameObject player in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            players.Add(player);
+        }
+        Debug.Log("There are " + players.Count + " players.");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        CheckForGameEnd();
+	}
+
+    void CheckForGameEnd()
+    {
+        //Player has entered a Door
+        if (triggerEntered)
+        {
+            //Safe has been looted
+            if (safeLooted)
+            {
+                RestartLevel();
+            }
+            triggerEntered = false;
+        }
+    }
+
+    void RestartLevel()
+    {
+        int scene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+
+        Debug.Log("Restarted Level");
+    }
+}
