2b8b463eb2b95d67f10def3544c1339f94454832
Kyle Taylor <kyle.jesse.taylor@gmail.com>
2018-08-28 13:15:09 +1000

    Added Simple Main Menu & Buttons

---
+++ StealthGame/Assets/Scripts/KyleMainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class KyleMainMenu : MonoBehaviour {
+
+    public GameObject inGameUI;
+    public GameObject mainMenuUI;
+
+	// Use this for initialization
+	void Start () {
+        //Freezes Game
+        Time.timeScale = 0;
+
+        //Turns off In-Game UI
+        inGameUI.SetActive(false);
+	}
+
+    public void StartGame()
+    {
+        //Resumes Time
+        Time.timeScale = 1;
+
+        //Turns on In-Game UI
+        inGameUI.SetActive(true);
+
+        //Turns of Main Menu UI
+        mainMenuUI.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
