4a816078e159e0195505d3b1335c30a972c0c30c
Kyle Taylor <kyle.jesse.taylor@gmail.com>
2018-08-28 14:21:56 +1000

    Main Menu Trans Done

--- StealthGame/Assets/Scripts/KyleMainMenu.cs
+++ StealthGame/Assets/Scripts/KyleMainMenu.cs
@@ -8,29 +8,59 @@ public class KyleMainMenu : MonoBehaviour {
     public GameObject inGameUI;
     public GameObject mainMenuUI;
 
-	// Use this for initialization
-	void Start () {
+    [Header("Cameras")]
+    public GameObject mainCamera;
+    public GameObject mainMenuCamera;
+
+    // Use this for initialization
+    void Start () {
         //Freezes Game
         Time.timeScale = 0;
 
         //Turns off In-Game UI
         inGameUI.SetActive(false);
+
+        //Turns off MM camera
+        mainCamera.SetActive(false);
 	}
+
+    private bool transitioning = false;
 
     public void StartGame()
     {
         //Resumes Time
         Time.timeScale = 1;
 
-        //Turns on In-Game UI
-        inGameUI.SetActive(true);
-
         //Turns of Main Menu UI
         mainMenuUI.SetActive(false);
+
+        TriggerAnimation();
     }
 
     public void ExitGame()
     {
         Application.Quit();
     }
+
+    void TriggerAnimation()
+    {
+        Animator anim = mainMenuCamera.GetComponent<Animator>();
+        anim.SetTrigger("CameraTrans");
+
+        StartCoroutine(ExecuteAfterTime(3.1f));
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        //suspend execution for X seconds
+        yield return new WaitForSeconds(time);
+
+        //Swaps Cinematic camera with Gameplay camera
+        mainCamera.SetActive(true);
+        mainMenuCamera.SetActive(false);
+        //Turns on In-Game UI
+        inGameUI.SetActive(true);
+    }
+
+    
 }
