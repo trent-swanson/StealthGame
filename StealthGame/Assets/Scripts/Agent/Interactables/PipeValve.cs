de4ef775a73c478e813eb5d7031bda2ff0db97d7
SamuelFenton <samuel.fenton@outlook.com>
2018-09-17 15:06:40 +1000

    Implemented pipe interactable Currently has no perfomring action

---
+++ StealthGame/Assets/Scripts/Agent/Interactables/PipeValve.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeValve : Interactable
+{
+    protected override void Start()
+    {
+        base.Start();
+
+        m_interactionNode = m_currentNode;
+
+    }
+
+    public override void PerformAction(Agent agent)
+    {
+        Debug.Log("Busting Pipe");
+    }
+}
