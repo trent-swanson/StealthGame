95622af768b383a5488bb7465f86cc89162393a3
Samuel Fenton <samuelfenton@outlook.com>
2018-11-07 01:16:28 +1100

    Door now has required player count

--- StealthGame/Assets/Scripts/Agent/Interactables/ExitDoor.cs
+++ StealthGame/Assets/Scripts/Agent/Interactables/ExitDoor.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class ExitDoor : Interactable
 {
+    public int m_requiredPlayerCount = 2;
     private GameState_PlayerTurn m_playerTurn = null;
 
     protected override void Start()
     {
         base.Start();
         m_playerTurn = m_gameController.GetComponent<GameState_PlayerTurn>();
+
+        if (m_interactionNodes.Count < m_requiredPlayerCount)
+            m_requiredPlayerCount = m_interactionNodes.Count;
     }
 
     public override bool CanPerform(Agent agent)
     {
-        return agent.m_agentInventory.AgentHasItem(m_requiredItem);
+        int playerCount = 0;
+        bool haveItem = false;
+
+        foreach (NavNode interactableNode in m_interactionNodes)
+        {
+            if (interactableNode.m_obstructingAgent != null && interactableNode.m_obstructingAgent.m_team == Agent.TEAM.PLAYER)
+            {
+                playerCount++;
+                if (agent.m_agentInventory.AgentHasItem(m_requiredItem))
+                    haveItem = true;
+            }
+        }
+
+        return haveItem && playerCount >= m_requiredPlayerCount;
     }
 
     public override void PerformAction(Agent agent)
